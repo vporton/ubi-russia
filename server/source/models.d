446ac49dccff6473679c660dec3b8d887a80790d
Victor Porton <porton@narod.ru>
2020-08-27 01:21:19 +0300

    persona model

---
+++ server/source/models.d
@@ -0,0 +1,19 @@
+import hunt.entity;
+import std.datetime.date;
+import utils.ethereum;
+
+@Table("person")
+class Person : Model
+{
+    mixin MakeModel;
+
+    @PrimaryKey
+    @AutoIncrement
+    uint id;
+
+    uint esiaID;
+    bool alive = true;
+    // DateTime birthDate;
+
+    // ETHAddress ethAddress; // can be changed by the user
+}
