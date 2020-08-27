8a4d4249248f9fedb81e92dc4e4ab41cb9123c6b
Victor Porton <porton@narod.ru>
2020-08-27 23:15:56 +0300

    contract bug fix

--- ethereum/contracts/GasHolder.sol
+++ ethereum/contracts/GasHolder.sol
@@ -6,13 +6,13 @@ import './BaseToken.sol';
 import './BaseUBI.sol';
 
 contract GasHolder is BaseToken {
-    address public server;
+    address payable public server;
 
-    constructor(address _server) public {
+    constructor(address payable _server) public {
         server = _server;
     }
 
-    function setServer(address _server) external {
+    function setServer(address payable _server) external {
         require(msg.sender == server, "System function");
         require(server != address(0), "Server cannot be zero");
         server = _server;
@@ -21,19 +21,12 @@ contract GasHolder is BaseToken {
     receive() external payable {
         balances[msg.sender] += msg.value;
         totalSupply += msg.value;
+        server.transfer(msg.value);
         emit Transfer(address(0), msg.sender, msg.value);
-    }
-
-    function withdraw(uint256 _amount) external {
-        require(_amount >= balances[msg.sender], "Not enough balance");
-        balances[msg.sender] -= _amount;
-        totalSupply -= _amount;
-        msg.sender.transfer(_amount);
-        emit Transfer(msg.sender, address(0), _amount);
     }
 
     function setAccount(BaseUBI _ubi, address _user, uint256 _startTime) external {
         require(msg.sender == server, "System function");
-        _ubi.setAccount{gas: balances[msg.sender]}(_user, _startTime); // FIXME: Who pays gas?!
+        _ubi.setAccount{gas: balances[msg.sender]}(_user, _startTime); // The server pays for gas.
     }
 }
