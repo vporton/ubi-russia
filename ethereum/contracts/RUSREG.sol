a0c4ff6030aa3e879d3621af077b2158ebda4a23
Victor Porton <porton@narod.ru>
2020-08-27 23:40:33 +0300

    RUSBIR.sol & RUSREG.sol

---
+++ ethereum/contracts/RUSREG.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.7.0;
+
+import './BaseUBI.sol';
+
+contract RUSREG is BaseUBI {
+    constructor(address _owner, address _gasHolder, uint8 _decimals, string memory _name, string memory _symbol)
+        BaseUBI(_owner, _gasHolder, 18, "Russian UBI Since Registration", "RUSREG")
+    { }
+}
