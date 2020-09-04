d7c4070aedd9f34a1facad306b25cab67ec83f1c
Victor Porton <porton@narod.ru>
2020-09-04 13:34:24 +0300

    removed server

--- server/src/schema.rs
+++
@@ -1,9 +0,0 @@
-table! {
-    persons (id) {
-        id -> Int8,
-        esiaid -> Int8,
-        alive -> Bool,
-        birthdate -> Timestamp,
-        ethaddress -> Nullable<Bpchar>,
-    }
-}
